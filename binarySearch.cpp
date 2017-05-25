04646f8cc1641fc2fe2759fbbcb8e4cb8dda3d30
Ali <strongsaxophone@gmail.com>
2017-05-25 19:13:34 +0430

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,13 +1,13 @@
 // recursive binarySearch
 int binarySearch(int goal, int *arr, int start, int end)
 {
-	int mid = (end-start)/2+start;
-	if(mid > e)
-		return -1;
-	if(arr[mid] == goal)
-		return  mid;
-	if(arr[mid] < goal)
-		return binarySearch(goal, arr, mid+1, end);
-	else
-		return binarySearch(goal, arr, start, mid-1);
+  int mid = (end-start)/2+start;
+  if(mid > e)
+    return -1;
+  if(arr[mid] == goal)
+    return  mid;
+  if(arr[mid] < goal)
+    return binarySearch(goal, arr, mid+1, end);
+  else
+    return binarySearch(goal, arr, start, mid-1);
 }
